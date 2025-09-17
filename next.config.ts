aafeb028b71f31548f804bf3df48e3e408ac32c6
Ryan Qahremani <ryanqahremanii@gmail.com>
2025-09-17 17:36:31 +0000

    make this website ready for static import

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type {NextConfig} from 'next';
 
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isGithubActions ? '/nextn' : '',
-  assetPrefix: isGithubActions ? '/nextn/' : '',
   typescript: {
     ignoreBuildErrors: true,
   },
